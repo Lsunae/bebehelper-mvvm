36259dadcba144cf47b3f9e78f365cb8c612d387
LEESUNAE <sjsao5012@gmail.com>
2023-01-03 22:42:46 +0900

    그룹핑 프래그먼트 생성

---
+++ app/src/main/java/com/example/bebehelper_mvvm/view/grouping/GroupingFragment.kt
@@ -0,0 +1,8 @@
+package com.example.bebehelper_mvvm.view.grouping
+
+import com.example.bebehelper_mvvm.R
+import com.example.bebehelper_mvvm.databinding.FragmentGroupingBinding
+import com.leesunae.bebehelper_mvp.view.base.BaseFragment
+
+class GroupingFragment : BaseFragment<FragmentGroupingBinding>(R.layout.fragment_grouping) {
+}
