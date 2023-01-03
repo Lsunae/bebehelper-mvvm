39037ac4f1f9dac02c5dfbc442f06186656bb323
LEESUNAE <sjsao5012@gmail.com>
2023-01-03 21:52:26 +0900

    메인 액티비티 위치 이동

--- app/src/main/java/com/example/bebehelper_mvvm/view/MainActivity.kt
+++ app/src/main/java/com/example/bebehelper_mvvm/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.bebehelper_mvvm
+package com.example.bebehelper_mvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bebehelper_mvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
