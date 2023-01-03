356e5c2c4840bcd70dcdfdd1eea3b4a3fc10030e
LEESUNAE <sjsao5012@gmail.com>
2023-01-03 22:45:28 +0900

    마이페이지 프래그먼트 생성

---
+++ app/src/main/java/com/example/bebehelper_mvvm/view/myPage/MyPageFragment.kt
@@ -0,0 +1,9 @@
+package com.example.bebehelper_mvvm.view.myPage
+
+import com.example.bebehelper_mvvm.R
+import com.example.bebehelper_mvvm.databinding.FragmentMyPageBinding
+import com.leesunae.bebehelper_mvp.view.base.BaseFragment
+
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+
+}
