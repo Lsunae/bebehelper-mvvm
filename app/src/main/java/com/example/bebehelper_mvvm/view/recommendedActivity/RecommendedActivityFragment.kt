a56908066370ec5197aacbaa78ff22396bd3b431
LEESUNAE <sjsao5012@gmail.com>
2023-01-03 22:45:04 +0900

    추천활동 프래그먼트 생성

---
+++ app/src/main/java/com/example/bebehelper_mvvm/view/recommendedActivity/RecommendedActivityFragment.kt
@@ -0,0 +1,9 @@
+package com.example.bebehelper_mvvm.view.recommendedActivity
+
+import com.example.bebehelper_mvvm.R
+import com.example.bebehelper_mvvm.databinding.FragmentRecomendedActivityBinding
+import com.leesunae.bebehelper_mvp.view.base.BaseFragment
+
+class RecommendedActivityFragment : BaseFragment<FragmentRecomendedActivityBinding>(R.layout.fragment_recomended_activity) {
+
+}
