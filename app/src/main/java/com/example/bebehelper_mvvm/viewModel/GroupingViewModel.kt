c3d4f048185678ee8a525cba83fa902355deb411
LEESUNAE <sjsao5012@gmail.com>
2023-01-23 00:25:35 +0900

    Grouping ViewModel 파일 추가

---
+++ app/src/main/java/com/example/bebehelper_mvvm/viewModel/GroupingViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.bebehelper_mvvm.viewModel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class GroupingViewModel : ViewModel() {
+}
