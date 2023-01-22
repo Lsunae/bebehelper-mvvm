a88e0c257f61339f86c55d19d4b2ee8bb68a0408
LEESUNAE <sjsao5012@gmail.com>
2023-01-22 23:41:36 +0900

    App 추가

---
+++ app/src/main/java/com/example/bebehelper_mvvm/App.kt
@@ -0,0 +1,21 @@
+package com.example.bebehelper_mvvm
+
+import android.app.Application
+import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    fun context(): Context = applicationContext
+
+    companion object {
+        lateinit var instance: App
+            private set
+
+    }
+}
