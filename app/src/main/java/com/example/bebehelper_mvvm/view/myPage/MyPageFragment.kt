b0a9866b05ab833f18b01530e0b794b160aa62be
LEESUNAE <sjsao5012@gmail.com>
2023-01-22 23:36:31 +0900

    패키지 명 변경

--- app/src/main/java/com/example/bebehelper_mvvm/view/myPage/MyPageFragment.kt
+++ app/src/main/java/com/example/bebehelper_mvvm/view/myPage/MyPageFragment.kt
@@ -2,7 +2,7 @@ package com.example.bebehelper_mvvm.view.myPage
 
 import com.example.bebehelper_mvvm.R
 import com.example.bebehelper_mvvm.databinding.FragmentMyPageBinding
-import com.leesunae.bebehelper_mvp.view.base.BaseFragment
+import com.example.bebehelper_mvvm.view.base.BaseFragment
 
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
 
--- app/src/main/java/com/example/bebehelper_mvvm/view/recommendedActivity/RecommendedActivityFragment.kt
+++ app/src/main/java/com/example/bebehelper_mvvm/view/recommendedActivity/RecommendedActivityFragment.kt
@@ -2,7 +2,7 @@ package com.example.bebehelper_mvvm.view.recommendedActivity
 
 import com.example.bebehelper_mvvm.R
 import com.example.bebehelper_mvvm.databinding.FragmentRecomendedActivityBinding
-import com.leesunae.bebehelper_mvp.view.base.BaseFragment
+import com.example.bebehelper_mvvm.view.base.BaseFragment
 
 class RecommendedActivityFragment : BaseFragment<FragmentRecomendedActivityBinding>(R.layout.fragment_recomended_activity) {
 
