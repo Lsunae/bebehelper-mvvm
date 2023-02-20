21fe93fc0fe188a30884ad63089ab77f9c021153
LEESUNAE <sjsao5012@gmail.com>
2023-02-20 23:27:35 +0900

    그룹핑 recyclerview adapter 데이터 추가 함수 구현 및 코드 정리

--- app/src/main/java/com/example/bebehelper_mvvm/view/grouping/adapter/GroupingAdapter.kt
+++ app/src/main/java/com/example/bebehelper_mvvm/view/grouping/adapter/GroupingAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bebehelper_mvvm.R
+import com.example.bebehelper_mvvm.data.model.GroupingItem
 import com.example.bebehelper_mvvm.databinding.ItemGroupingBinding
 import com.example.bebehelper_mvvm.view.grouping.GroupingFragment
 import com.example.bebehelper_mvvm.util.Utils
@@ -12,8 +13,14 @@ import java.lang.ref.WeakReference
 
 class GroupingAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private lateinit var context: Context
-    private var items = mutableListOf<com.example.bebehelper_mvvm.data.room.entity.Grouping>()
+    private var items = mutableListOf<GroupingItem>()
     lateinit var groupingFragment: WeakReference<GroupingFragment>
+
+    fun addData(addDataList: List<GroupingItem>) {
+        this.items.clear()
+        items.addAll(addDataList)
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         this.context = parent.context
@@ -49,9 +56,9 @@ class GroupingAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     inner class GroupingHolder(private val binding: ItemGroupingBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        private lateinit var item: com.example.bebehelper_mvvm.data.room.entity.Grouping
+        private lateinit var item: GroupingItem
 
-        fun bind(item: com.example.bebehelper_mvvm.data.room.entity.Grouping, position: Int) {
+        fun bind(item: GroupingItem, position: Int) {
             this.item = item
 
             binding.apply {
