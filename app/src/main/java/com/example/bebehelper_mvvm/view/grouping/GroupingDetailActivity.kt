c4187d04009b57c2bd07592f6fe500d05ae70843
LEESUNAE <sjsao5012@gmail.com>
2023-03-10 19:19:16 +0900

    그룹핑 글 상세 클래스 생성

---
+++ app/src/main/java/com/example/bebehelper_mvvm/view/grouping/GroupingDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bebehelper_mvvm.view.grouping
+
+import android.os.Bundle
+import com.example.bebehelper_mvvm.R
+import com.example.bebehelper_mvvm.databinding.ActivityGroupingCreateBinding
+import com.example.bebehelper_mvvm.view.base.BaseActivity
+
+class GroupingDetailActivity : BaseActivity<ActivityGroupingCreateBinding>(R.layout.activity_grouping_create) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
