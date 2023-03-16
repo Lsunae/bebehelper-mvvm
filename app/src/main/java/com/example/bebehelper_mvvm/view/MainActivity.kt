c350bc81f6a113eada12cf438d4ad5b1f9b59a03
LeeSunAe <lsunae@LeeSunAeui-MacBookAir.local>
2023-03-16 19:40:00 +0900

    오류 수정 및 코드 정리

--- app/src/main/java/com/example/bebehelper_mvvm/view/MainActivity.kt
+++ app/src/main/java/com/example/bebehelper_mvvm/view/MainActivity.kt
@@ -5,7 +5,6 @@ import com.example.bebehelper_mvvm.R
 import com.example.bebehelper_mvvm.constant.MenuType
 import com.example.bebehelper_mvvm.databinding.ActivityMainBinding
 import com.example.bebehelper_mvvm.view.grouping.GroupingFragment
-import com.example.bebehelper_mvvm.view.myPage.MyPageFragment
 import com.example.bebehelper_mvvm.view.recommendedActivity.RecommendedActivityFragment
 import com.example.bebehelper_mvvm.view.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,25 +16,16 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         initNavigationBar()
     }
 
     private fun initNavigationBar() {
-        println("initNavigationBar_ ")
         binding.bottomNavigationView.run {
             setOnItemSelectedListener {
-                println("initNavigationBar_ ${it.itemId}")
                 when (it.itemId) {
-                    R.id.action_grouping -> {
-                        println("initNavigationBar_grouping_ ${it.itemId}")
-                        changeFragment(MenuType.GROUPING)
-                    }
-                    R.id.action_recommended_activity -> {
-                        println("initNavigationBar_recommended_ ${it.itemId}")
-                        changeFragment(MenuType.RECOMMENDED_ACTIVITY)
-                    }
+                    R.id.action_grouping -> changeFragment(MenuType.GROUPING)
+                    R.id.action_recommended_activity -> changeFragment(MenuType.RECOMMENDED_ACTIVITY)
                 }
                 true
             }
@@ -47,18 +37,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private fun selectMenuTabIndex(type: MenuType?) {
         val navigation = binding.bottomNavigationView
         runOnUiThread {
-            println("select_tab_ $type")
             when (type) {
-                MenuType.RECOMMENDED_ACTIVITY -> {
-
-                    println("select_tab_1 $type")
-                    navigation.selectedItemId = R.id.action_recommended_activity
-                }
-                else -> {
-
-                    println("select_tab_2 $type")
-                    navigation.selectedItemId = R.id.action_grouping
-                }
+                MenuType.RECOMMENDED_ACTIVITY -> navigation.selectedItemId =
+                    R.id.action_recommended_activity
+                else -> navigation.selectedItemId = R.id.action_grouping
             }
         }
     }
@@ -68,11 +50,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
      * 각 프래그먼트 화면 유지를 위해 hide, show 사용
      */
     private fun changeFragment(type: MenuType) {
-        println("type_ $type")
         when (type) {
             MenuType.GROUPING -> {
-                println("type_groupingFragment_ $groupingFragment")
-                println("type_1 $type")
                 if (groupingFragment == null) {
                     groupingFragment = GroupingFragment()
                     supportFragmentManager.beginTransaction()
@@ -85,17 +64,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                     .hide(recommendedActivityFragment!!).commit()
             }
             MenuType.RECOMMENDED_ACTIVITY -> {
-                println("type_recommendedActivityFragment_ $recommendedActivityFragment")
-                println("type_2 $type")
                 if (recommendedActivityFragment == null) {
                     recommendedActivityFragment = RecommendedActivityFragment()
                     supportFragmentManager.beginTransaction()
                         .add(binding.mainContainer.id, recommendedActivityFragment!!).commit()
                 } else {
-                    supportFragmentManager.beginTransaction().show(recommendedActivityFragment!!).commit()
+                    supportFragmentManager.beginTransaction().show(recommendedActivityFragment!!)
+                        .commit()
                     recommendedActivityFragment!!.onResume()
                 }
-
                 if (groupingFragment != null) supportFragmentManager.beginTransaction()
                     .hide(groupingFragment!!).commit()
             }
